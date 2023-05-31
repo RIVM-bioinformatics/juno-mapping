14ae8a561060ad0fd8fa1ab6f8bbc4ca7978fb7c
boasvdp <boasvdp@gmail.com>
2023-05-31 09:42:09 +0200

    feat: add options to wrapper

--- juno-mapping.py
+++ juno-mapping.py
@@ -26,32 +26,80 @@ class JunoMapping(Pipeline):
     pipeline_name: str = __package_name__
     pipeline_version: str = __version__
     input_type: str = "fastq"
+    species_options = ["mycobacterium_tuberculosis"]
 
     def _add_args_to_parser(self) -> None:
         super()._add_args_to_parser()
 
-        self.parser.description = "Juno-mapping pipeline"
+        self.parser.description = "Juno-mapping pipeline for reference mapping of bacterial genomes"
         
         self.add_argument(
-            "--example-option",
-            dest="example",
-            type=str,
-            required=False,
+            "-s",
+            "--species",
+            dest="species",
+            type=str.lower,
+            required=True,
             metavar="STR",
-            help="This is an optional argument, specific for this pipeline. General options are included in juno-library.",
+            help=f"Species to use, choose from: {self.species_options}",
+            choices=self.species_options
         )
+        self.add_argument(
+            "--reference",
+            type=Path,
+            metavar="FILE",
+            help="Reference genome to use. Default is chosen based on species argument, defaults per species can be found in: /mnt/db/juno/mapping/[species]",
+            required=False
+        )
+        self.add_argument(
+            "--db-dir",
+            type=Path,
+            default="/mnt/db/juno/kraken2_db",
+            metavar="DIR",
+            help="Kraken2 database directory.",
+        )
+        self.add_argument(
+             "-mpt",
+            "--mean-quality-threshold",
+            type = int,
+            metavar = "INT",
+            default = 28,
+            help = "Phred score to be used as threshold for cleaning (filtering) fastq files."
+        )
+        self.add_argument(
+            "-ws",
+            "--window-size",
+            type = int,
+            metavar = "INT",
+            default = 5,
+            help = "Window size to use for cleaning (filtering) fastq files."
+        )
+        self.add_argument(
+            "-ml",
+            "--minimum-length",
+            type = int,
+            metavar = "INT",
+            default = 50,
+            help = "Minimum length for fastq reads to be kept after trimming."
+        )
+
         
     def _parse_args(self) -> argparse.Namespace:
         args = super()._parse_args()
 
         # Optional arguments are loaded into self here
-        self.example: bool = args.example
+        self.db_dir: Path = args.db_dir
+        self.mean_quality_threshold: int = args.mean_quality_threshold
+        self.window_size: int = args.window_size
+        self.min_read_length: int = args.minimum_length
+        self.reference: Path = args.reference
+        self.time_limit: int = args.time_limit
+        self.species: str = args.species
 
         return args
     
-    # Extra class methods for this pipeline can be defined here
-    def example_class_method(self):
-        print(f"example option is set to {self.example}")
+    # # Extra class methods for this pipeline can be defined here
+    # def example_class_method(self):
+    #     print(f"example option is set to {self.example}")
 
     def setup(self) -> None:
         super().setup()
@@ -59,13 +107,23 @@ class JunoMapping(Pipeline):
         if self.snakemake_args["use_singularity"]:
             self.snakemake_args["singularity_args"] = " ".join(
                 [
-                    self.snakemake_args["singularity_args"]
+                    self.snakemake_args["singularity_args"],
+                    f"--bind {self.db_dir}:{self.db_dir}",
                 ] # paths that singularity should be able to read from can be bound by adding to the above list
             )
 
         # Extra class methods for this pipeline can be invoked here
         if self.example:
             self.example_class_method()
+        
+        # select a reference based on species:
+        #self.ref_dir = "/mnt/db/apollo/mapping/"
+        if self.species == "mycobacterium_tuberculosis":
+            self.reference = "/mnt/db/juno/mapping/mycobacterium_tuberculosis/AL123456.3.fasta"
+        # elif self.species == "speciesX":
+        #     self.reference = "/mnt/db/juno/mapping/speciesX/awesome_assembly.fasta"
+
+        print("Running pipeline for " + self.species + " with reference: " + str(self.reference))
 
         with open(
             Path(__file__).parent.joinpath("config/pipeline_parameters.yaml")
@@ -77,9 +135,14 @@ class JunoMapping(Pipeline):
             "input_dir": str(self.input_dir),
             "output_dir": str(self.output_dir),
             "exclusion_file": str(self.exclusion_file),
-            "example": str(self.example), # other user parameters can be included in user_parameters.yaml here
+            "db_dir": str(self.db_dir),
+            "mean_quality_threshold": int(self.mean_quality_threshold),
+            "window_size": int(self.window_size),
+            "min_read_length": int(self.min_read_length),
+            "reference": str(self.reference),
+            "use_singularity": str(self.snakemake_args['use_singularity']),
+            "species": str(self.species),
         }
-
 
 if __name__ == "__main__":
     main()
