b368a64957d2f9a090fd1819b759f77278d6353d
Boas van der Putten <32385917+boasvdp@users.noreply.github.com>
2023-06-12 11:10:56 +0200

    fix: rework mamba env activation

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+set -x
+
 #----------------------------------------------#
 # User parameters
 if [ ! -z "${1}" ] || [ ! -z "${2}" ] || [ ! -z "${irods_input_projectID}" ]
@@ -30,25 +32,34 @@ else
 fi
 
 #----------------------------------------------#
-# Create/update necessary environments
-PATH_MAMBA_YAML="envs/mamba.yaml"
-PATH_MASTER_YAML="envs/juno_mapping.yaml"
-MAMBA_NAME=$(head -n 1 ${PATH_MAMBA_YAML} | cut -f2 -d ' ')
-MASTER_NAME=$(head -n 1 ${PATH_MASTER_YAML} | cut -f2 -d ' ')
+## make sure conda works
 
-echo -e "\nUpdating necessary environments to run the pipeline..."
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/mnt/miniconda/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/mnt/miniconda/etc/profile.d/conda.sh" ]; then
+        . "/mnt/miniconda/etc/profile.d/conda.sh"
+    else
+        export PATH="/mnt/miniconda/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<export -f conda
+export -f __conda_activate
+export -f __conda_reactivate
+export -f __conda_hashr
 
-# Removing strict mode because it sometimes breaks the code for 
-# activating an environment and for testing whether some variables
-# are set or not
-set +euo pipefail 
 
-conda env update -f "${PATH_MAMBA_YAML}"
-source activate "${MAMBA_NAME}"
+#----------------------------------------------#
+# Create the environment
 
-mamba env update -f "${PATH_MASTER_YAML}"
-
-source activate "${MASTER_NAME}"
+# we can use the base installation of mamba to create the environment. 
+# Swapping to a parent env is not necessary anymore.
+mamba env create -f envs/juno_mapping.yaml --name pipeline_env
+conda activate pipeline_env
 
 #----------------------------------------------#
 # Run the pipeline
@@ -73,8 +84,6 @@ else
     QUEUE="bio"
 fi
 
-set -euo pipefail
-set -x
 
 # Setting up the tmpdir for singularity as the current directory (default is /tmp but it gets full easily)
 # Containers will use it for storing tmp files when building a container
